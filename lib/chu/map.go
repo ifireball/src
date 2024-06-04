1bc8958d7b73e648375eaa8a42ec997d7a65734d
Barak Korren <bkorren@redhat.com>
2024-06-04 15:51:19 +0300

    chu.Map: Run in parallel
    Signed-off-by: Barak Korren <bkorren@redhat.com>

--- lib/chu/map.go
+++ lib/chu/map.go
@@ -2,11 +2,22 @@ package chu
 
 func Map[inT, outT interface{}](ch <-chan inT, f func(inT) (outT, bool)) <-chan outT {
 	out := make(chan outT)
+	collector := make(chan struct{out outT; keep bool})
 	go func() {
 		defer func() { close(out) }()
+		defer func() { close(collector) }()
+		count := 0
 		for o := range ch {
-			if oo, keep := f(o); keep {
-				out <- oo
+			go func(o inT) {
+				oo, keep := f(o)
+				collector <- struct{out outT; keep bool}{oo, keep}
+			}(o)
+			count++
+		}
+		for i := 0; i < count; i++ {
+			cs := <-collector
+			if cs.keep {
+				out <- cs.out
 			}
 		}
 	}()
