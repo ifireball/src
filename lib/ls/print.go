fe0bc172dff4de806a6d57b139fe382d49a433cc
Barak Korren <bkorren@redhat.com>
2024-06-05 14:56:12 +0300

    ls: print: sort and align
    Signed-off-by: Barak Korren <bkorren@redhat.com>

--- lib/ls/print.go
+++ lib/ls/print.go
@@ -1,9 +1,28 @@
 package ls
 
-import "fmt"
+import (
+	"fmt"
+	"slices"
+	"strings"
+)
+
+type tableRow struct{name, since string}
 
 func Print(repos <-chan Repo) {
+	var table []tableRow
+	nameWidth := 0
 	for repo := range repos {
-		fmt.Printf("%s/%s/%s @ %s\n", repo.Host, repo.Org, repo.Name, repo.LastCommitTime)
+		name := fmt.Sprintf("%s/%s/%s", repo.Host, repo.Org, repo.Name)
+		nameWidth = max(nameWidth, len(name))
+		table = append(table, struct{name string; since string}{
+			name,
+			repo.LastCommitTime.Format("Mon, Jan _2 2006"),
+		})
+	}
+	slices.SortFunc(table, func(a, b tableRow) int {
+		return strings.Compare(a.name, b.name)
+	})
+	for _, row := range table {
+		fmt.Printf("%-[1]*[2]s @ %[3]s\n", nameWidth, row.name, row.since)
 	}
 }
